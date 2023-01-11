276f95988decf448b6008dfe72852c93bf2b1c1e
austinltam <tamaust@isu.edu>
2023-01-10 17:09:14 -0700

    Updated EFAfragment2D to use simple split in single cut cases and fixed node test error

--- modules/xfem/src/efa/EFAElement2D.C
+++ modules/xfem/src/efa/EFAElement2D.C
@@ -843,7 +843,7 @@ EFAElement2D::updateFragments(const std::set<EFAElement *> & CrackTipElements,
 
   unsigned int num_frag_edges = _fragments[0]->numEdges();
 
-  if (num_cut_frag_edges == 0)
+  if (num_cut_frag_edges == 0 && num_cut_nodes == 0)
   {
     if (!isPartial()) // delete the temp frag for an uncut elem
     {
@@ -878,8 +878,6 @@ EFAElement2D::updateFragments(const std::set<EFAElement *> & CrackTipElements,
 
     fragmentSanityCheck(num_frag_edges, num_cut_frag_edges);
   }
-  // else if (num_cut_frag_edges == 1 && num_cut_nodes == 0)
-  //   new_frags = _fragments[0]->split();
   else
   {
     new_frags = _fragments[0]->split(EmbeddedNodes);
